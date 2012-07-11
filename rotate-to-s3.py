27f5fd67a9b1d8b02286abc899bb52dcc45ad4d6
dialt0ne <dialt0ne@yahoo.com>
2012-07-11 11:09:49 -0400

    added license

--- rotate-to-s3.py
+++ rotate-to-s3.py
@@ -3,6 +3,22 @@
 # rotate-to-s3.py
 #
 # ATonns Wed Mar 14 19:40:57 UTC 2012
+#
+#   Copyright 2012 Corsis
+#   http://www.corsis.com/
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 import argparse
 import boto
 import gzip
